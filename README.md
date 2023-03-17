5ffa7393513c62636dd0db475c774256887895f6
Hirozuto <madeayurenapramudya@gmail.com>
2023-03-17 14:56:19 +0000

    Add extras for a03snnxx-user 13 TP1A.220624.014 A037FXXS2CWA3 release-keys
    Signed-off-by: Hirozuto <madeayurenapramudya@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## a03snnxx-user 13 TP1A.220624.014 A037FXXS2CWA3 release-keys
+- Manufacturer: samsung
+- Platform: mt6765
+- Codename: a03s
+- Brand: samsung
+- Flavor: a03snnxx-user
+- Release Version: 13
+- Id: TP1A.220624.014
+- Incremental: A037FXXS2CWA3
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: undefined
+- Fingerprint: samsung/a03snnxx/a03s:12/SP1A.210812.016/A037FXXS2CWA3:user/release-keys
+- OTA version: 
+- Branch: a03snnxx-user-13-TP1A.220624.014-A037FXXS2CWA3-release-keys
+- Repo: samsung_a03s_dump
+
+
+>Dumped by [yukiprjkt](https://t.me/shirayuki_plygrnd)
---
+++ join_split_files.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cat system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null >> system/system/priv-app/SecSettings/SecSettings.apk
+rm -f system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null
+cat system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null >> system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk
+rm -f system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null
+cat system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null >> system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk
+rm -f system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null
+cat system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system/system/system_ext/apex/com.android.vndk.v30.apex
+rm -f system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat system/system/framework/framework-res.apk.* 2>/dev/null >> system/system/framework/framework-res.apk
+rm -f system/system/framework/framework-res.apk.* 2>/dev/null
+cat system/system/framework/oat/arm64/services.odex.* 2>/dev/null >> system/system/framework/oat/arm64/services.odex
+rm -f system/system/framework/oat/arm64/services.odex.* 2>/dev/null
+cat system/system/app/HoneyBoard/HoneyBoard.apk.* 2>/dev/null >> system/system/app/HoneyBoard/HoneyBoard.apk
+rm -f system/system/app/HoneyBoard/HoneyBoard.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/Phonesky/Phonesky.apk.* 2>/dev/null >> product/priv-app/Phonesky/Phonesky.apk
+rm -f product/priv-app/Phonesky/Phonesky.apk.* 2>/dev/null
+cat product/priv-app/Messages/Messages.apk.* 2>/dev/null >> product/priv-app/Messages/Messages.apk
+rm -f product/priv-app/Messages/Messages.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk.* 2>/dev/null >> product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk
+rm -f product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
